aef0520937e926b33f35aac0fd0e156e1d58f52d
Charlie <charliemcgowan96@gmail.com>
2020-08-01 19:31:31 +0100

    letters update

--- main.py
+++ main.py
@@ -46,45 +46,59 @@ class NumberRound:
 		self.numbers.append(x)
 		#return x
 
-new_game = NumberRound()
-new_game.change_target()
+# new_game = NumberRound()
+# new_game.change_target()
 
-new_game.add_number(False)
-new_game.add_number(False)
-new_game.add_number(False)
-new_game.add_number(False)
-new_game.add_number(False)
-new_game.add_number(True)
-print(new_game.numbers)
-print(new_game.target)
+# new_game.add_number(False)
+# new_game.add_number(False)
+# new_game.add_number(False)
+# new_game.add_number(False)
+# new_game.add_number(False)
+# new_game.add_number(True)
+# print(new_game.numbers)
+# print(new_game.target)
 
 
 # I need
 # letters
 # return letters
+# Need to split vowels and consonants
 
-# class WordRound:
-# 	# A number round
-# 	def __init__(self):
-# 		# Create the numbers based on weights
-# 		self.letter_weights = [ ('A',15), ('B',2), ('C',3), ('D',6), ('E',21), ('F',2), ('G', 3),
-# 								('H',2), ('I',13),  ('J',1), ('K',1), ('L',5), ('M',4), ('N',8), 
-# 								('O',13), ('P',4), ('Q',1), ('R',9), ('S',9), ('T',9), ('U',5), 
-# 								('V',1), ('W',1), ('X',1), ('Y',1), ('Z',1)]
+class WordRound:
+	# A number round
+	def __init__(self):
+		# Create the numbers based on weights
+		self.letter_weights = [ ('A',15), ('B',2), ('C',3), ('D',6), ('E',21), ('F',2), ('G', 3),
+								('H',2), ('I',13),  ('J',1), ('K',1), ('L',5), ('M',4), ('N',8), 
+								('O',13), ('P',4), ('Q',1), ('R',9), ('S',9), ('T',9), ('U',5), 
+								('V',1), ('W',1), ('X',1), ('Y',1), ('Z',1)]
 
-# 		# Loop to create a weighted list
-# 		self.letters = []
-# 		for item, weight in letter_weights:
-# 		    self.letters.extend( [item]*weight )
-# 		self.letters = np.array(self.letters)	
-
-
-# 	def add_letter(self):
-# 		# Index of target element
-# 		idx = np.where(letters == letter)[0][0]
-# 		letters = np.delete(letters, idx)
+		# Loop to create a weighted list
+		self.letters_list = []
+		for item, weight in self.letter_weights:
+		    self.letters_list.extend( [item]*weight )
+		self.letters_list = np.array(self.letters_list)	
+		# List to store letters
+		self.letters = []
 
 
+	def add_letter(self):
+		# Selecting a letter
+		letter = random.choice(self.letters_list)
+		# Index of target element
+		idx = np.where(self.letters_list == letter)[0][0]
+		# Remove letter from list
+		self.letters_list = np.delete(self.letters_list, idx)
+		# Store the letter
+		self.letters.append(letter)
+
+
+
+word = WordRound()
+word.add_letter()
+word.add_letter()
+word.add_letter()
+print(word.letters)
 
 
 # letter = random.choice( letters )
